e10d1a857ea50cbdbc50f17e08a5a6e2abd07627
ITIT <ITIT@iMac-de-ULSA.local>
2025-04-03 19:01:27 -0700

    Se agrega el examen de segundo parcial

--- CookBook/SecondPartial/WaterTracker/Views/MainTracker.swift
+++ CookBook/SecondPartial/WaterTracker/Views/MainTracker.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct MainTracker: View {
+    
+    @StateObject private var viewModel = AuthenticationViewModel()
+    
+    
+    @State private var navigateToWaterTracker = false
+    
     var body: some View {
+        
         NavigationView{
             VStack{
                 Text("What do you want to do?")
                     .font(.system (size: 30))
                 
+                
+                Button(action: {
+                    viewModel.authenticateWithFaceID()
+                }){
+                    Text("Go to Water Tracker")
+                        .padding(20)
+                        .font(.system (size: 25))
+                        .foregroundColor(.white)
+                        .background(
+                            RoundedRectangle(cornerRadius: 200)
+                                .fill(Color.blue)
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                
+                NavigationLink(
+                    destination: TrackerView(),
+                    isActive: $navigateToWaterTracker,
+                    label: { EmptyView() }
+                )
                 
                 NavigationLink(destination: WaterTrackerView()){
                     Text("Go to Profile")
@@ -22,23 +49,20 @@ struct MainTracker: View {
                         .foregroundColor(.white)
                         .background(
                             RoundedRectangle(cornerRadius: 200)
-                                .fill(Color.blue)
+                                .fill(Color.green)
                         )
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
                 
-                NavigationLink(destination: TrackerView()){
-                    Text("Go to WaterTracker")
-                        .padding(20)
-                        .font(.system (size: 25))
-                        .foregroundColor(.white)
-                        .background(
-                            RoundedRectangle(cornerRadius: 200)
-                                .fill(Color.blue)
-                        )
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                }
             }
+            .onAppear {
+                viewModel.isAuthenticated = false
+            }
+            .onChange(of: viewModel.isAuthenticated) { isAuthenticated in
+                            if isAuthenticated {
+                                navigateToWaterTracker = true
+                            }
+                        }
         }
     }
 }
--- CookBook/SecondPartial/WaterTracker/WaterTrackerViewModel/WaterTrackerViewModel.swift
+++ CookBook/SecondPartial/WaterTracker/WaterTrackerViewModel/WaterTrackerViewModel.swift
@@ -1,8 +1,12 @@
 
 import Foundation
+import LocalAuthentication
+import SwiftUI
 
 class WaterTrackerViewModel: ObservableObject{
     @Published var Total = 0
+    @State private var isUserAuthenticated = false
+    @State private var showAlert = false
     
     func aumcuarter (){
         Total += 255
